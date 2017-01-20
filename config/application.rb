fa432362b0a71d7543b48fdb3c6770c1cc511777
Carlie Clippinger <carliesachiko@gmail.com>
2017-01-19 18:45:00 -0800

    fixing images problem

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
+config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
