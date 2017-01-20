c4bbe9e860a38aa0b9c8f82cc9863a7a3ba1f506
Carlie Clippinger <carliesachiko@gmail.com>
2017-01-19 16:14:06 -0800

    final touches

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def randomized_background_image
+    images = ["assets/IMG_8794.JPG", "assets/IMG_8780.JPG", "assets/IMG_0055.jpg", "assets/IMG_8777-2.jpg" ]
+    images[rand(images.size)]
+  end
+
 end
