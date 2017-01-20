e1a50ed765682906323477093c4a580d1d664cf5
Carlie Clippinger <carliesachiko@gmail.com>
2017-01-19 21:54:55 -0800

    heroku to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_many :invited_trips, through: :user_trips, source: :trip
 
   has_secure_password
-  validates :email, presence: true, uniqueness: true, :length => {:within => 6..40}
+  validates :email, presence: true, uniqueness: true
 
   has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
